a553eb2951dd46f2d142b770ab408a93f9881d2b
demmyhafiz <hafiz.janitra@gmail.com>
2019-10-06 22:33:21 +0700

    create 2 activity and show text message from main activity

--- app/src/main/java/co/janitra/calculatormaterialdesign/MainActivity.java
+++ app/src/main/java/co/janitra/calculatormaterialdesign/MainActivity.java
@@ -2,13 +2,26 @@ package co.janitra.calculatormaterialdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "co.janitra.calculatormaterialdesign.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+    /** this calling Send button when the user taps on it */
+    public void sendMessage(View view) {
+        // this will do something when Send button taps by user
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+    }
